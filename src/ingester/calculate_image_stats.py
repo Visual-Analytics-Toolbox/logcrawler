fix: image stats calculation

src/ingester/calculate_image_stats.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from tqdm import tqdm
 import numpy as np
 import cv2
 
@@ -16,15 +15,13 @@ def calculate_image_stats(log_root_path, client):
     def sort_key_fn(log):
         return log.id
 
-    # FIXME how to query for a given log?
     for log in sorted(logs, key=sort_key_fn, reverse=True):
         print(f"{log.id}: {log.log_path}")
 
         images = client.image.list(log=log.id, blurredness_value="None")
 
-        image_data = list()
-
-        for idx, img in enumerate(tqdm(images)):
+        image_update_data = list()
+        for idx, img in enumerate(images):
             image_path = Path(log_root_path) / img.image_url
             image_cv = cv2.imread(image_path, cv2.IMREAD_COLOR)
 
@@ -32,20 +29,18 @@ def calculate_image_stats(log_root_path, client):
                 gray = cv2.cvtColor(image_cv, cv2.COLOR_BGR2GRAY)
                 brightness_value = np.average(gray)
                 blurredness_value = variance_of_laplacian(gray)
-                height, width, channels = image_cv.shape
 
                 json_obj = {
                     "id": img.id,
                     "blurredness_value": blurredness_value,
                     "brightness_value": brightness_value,
-                    "resolution": f"{width}x{height}x{channels}",
                 }
 
-                image_data.append(json_obj)
+                image_update_data.append(json_obj)
 
             except Exception as e:
                 print(e)
-                print(f"Image broken at {url} in log: {log.id}")
+                print(f"Image broken at {img.image_url} in log: {log.id}")
                 print(
                     "This problem can occur if the image extraction for this log was aborted"
                 )
@@ -53,16 +48,17 @@ def calculate_image_stats(log_root_path, client):
 
             if idx % 100 == 0 and idx != 0:
                 try:
-                    response = client.image.bulk_update(data=image_data)
-                    image_data.clear()
+                    response = client.image.bulk_update(data=image_update_data)
+                    image_update_data.clear()
+                    print(idx)
                 except Exception as e:
                     print(e)
                     print("error inputing the data")
                     quit()
 
-        if len(image_data) > 0:
+        if len(image_update_data) > 0:
             try:
-                response = client.image.bulk_update(data=image_data)
+                response = client.image.bulk_update(data=image_update_data)
             except Exception as e:
                 print(e)
                 print("error inputing the data")