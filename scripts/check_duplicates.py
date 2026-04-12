1ae86379393e16cf6a6c9d383529c79603f37cca
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2026-04-12 23:13:24 +0200

    add test that checks for duplicated logs

---
+++ scripts/check_duplicates.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from datetime import datetime
+from vaapi.client import Vaapi
+import logging
+import os
+
+
+def has_duplicates(data_list, fieldname):
+    # Create a set of all values for that specific key
+    values = [d[fieldname] for d in data_list if fieldname in d]
+    return len(values) != len(set(values))
+
+if __name__ == "__main__":
+    v_client = Vaapi(
+        base_url=os.environ.get("VAT_API_URL"),
+        api_key=os.environ.get("VAT_API_TOKEN"),
+    )
+    #test("/mnt/repl", v_client)
+    logs = list(v_client.logs.list())
+
+    print(has_duplicates(logs, "log_path"))  # Output: True
